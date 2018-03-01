299f80cd9c152d79d87dd1f272805bf7349426f6
ChildishhMacbino <connorwilliams.home@gmail.ocm>
2018-03-02 10:39:44 +1100

    Comment Main

--- src/rpgquest/RPGQuest.java
+++ src/rpgquest/RPGQuest.java
@@ -15,6 +15,7 @@ public class RPGQuest {
     /**
      * @param args the command line arguments
      */
+    // this is the main method
     public static void main(String[] args) {
         // TODO code application logic here
         game = new Game();
